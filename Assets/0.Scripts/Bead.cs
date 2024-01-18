2db7ac5d1b03eb1aad6571cd34dff2ee1e8874df
xyl_t9 <dbsrjs1224@gmail.com>
2024-01-18 17:53:11 +0900

    01/18 Academy

--- Assets/0.Scripts/Bead.cs
+++ Assets/0.Scripts/Bead.cs
@@ -14,73 +14,93 @@ public enum BeadType
 
 public class Bead : MonoBehaviour
 {
+    public PotionType potionType;
+
     [SerializeField] private List<Sprite> sprite;
 
 
-    Vector2 startPos;
+    Vector2 startPos = new();
     Collider2D target = null;
+
+    public Vector2 clampVec2;
+    private bool isMoving = false;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))    //마우스를 눌렀을 때
         {
-            if (GetHit2D().collider != null)
+            if (GetHit2D().collider != null && isMoving != true)
             {
-                startPos = transform.position;
                 target = GetHit2D().collider;
+                startPos = transform.position;
+                isMoving = true;
+                Debug.Log("startPos : " + startPos);
+                Debug.Log("potionType : " + potionType);
             }
         }
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0))    //마우스를 누르고 있을 때
         {
             if (target != null)
             {
                 Vector2 vec = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
                 vec = Camera.main.ScreenToWorldPoint(vec);
-                //transform.position = vec;
+
+                // 위치 제한을 위해 Mathf.Clamp 사용
+                float clampedX = Mathf.Clamp(vec.x, -(clampVec2.x), clampVec2.x);
+                float clampedY = Mathf.Clamp(vec.y, -(clampVec2.y), clampVec2.y);
+                vec = new Vector2(clampedX, clampedY);
 
                 if((vec - startPos).normalized.x > 0 && ((vec - startPos).normalized.y > -0.5f &&   //오른쪽
                     (vec - startPos).normalized.y < 0.5f))
                 {
-                    transform.position = new Vector2(vec.x, transform.position.y);
-                    Debug.Log("오른쪽으로 이동");
+                    target.transform.position = new Vector2(vec.x, transform.position.y);
+                    //Debug.Log("오른쪽으로 이동");
                 }
 
                 else if ((vec - startPos).normalized.x < 0 && ((vec - startPos).normalized.y > -0.5f && //왼쪽
                     (vec - startPos).normalized.y < 0.5f))
                 {
-                    transform.position = new Vector2(vec.x, transform.position.y);
-                    Debug.Log("왼쪽으로 이동");
+                    target.transform.position = new Vector2(vec.x, transform.position.y);
+                    //Debug.Log("왼쪽으로 이동");
                 }
 
                 else if ((vec - startPos).normalized.y > 0 && ((vec - startPos).normalized.x > -0.5f && //위쪽
                     (vec - startPos).normalized.x < 0.5f))
                 {
-                    transform.position = new Vector2(transform.position.x, vec.y);
-                    Debug.Log("위쪽으로 이동");
+                    target.transform.position = new Vector2(transform.position.x, vec.y);
+                    //Debug.Log("위쪽으로 이동");
                 }
 
                 else if ((vec - startPos).normalized.y < 0 && ((vec - startPos).normalized.x > -0.5f && //아래쪽
                    (vec - startPos).normalized.x < 0.5f))
                 {
-                    transform.position = new Vector2(transform.position.x, vec.y);
-                    Debug.Log("아래쪽으로 이동");
+                    target.transform.position = new Vector2(transform.position.x, vec.y);
+                    //Debug.Log("아래쪽으로 이동");
                 }
 
-                Debug.Log((vec - startPos).normalized);
+                //Debug.Log((vec - startPos).normalized);
             }
         }
 
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0))  //마우스를 눌렀다가 놓았을 때
         {
-            transform.position = startPos;
-            target = null;
+                transform.position = startPos;
+                isMoving = false;
+                {
+                if (isMoving != true)
+                    target = null;
+
+                }
         }
     }
 
     RaycastHit2D GetHit2D()
     {
+        // 현재 마우스 위치를 스크린 좌표로부터 레이로 변환
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // 레이캐스트를 통해 레이와 충돌한 객체에 대한 정보 저장
         RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
 
         return hit;
