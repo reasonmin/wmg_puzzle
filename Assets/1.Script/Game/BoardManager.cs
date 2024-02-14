ac4bbd546c9c73fa46a674245ec020a0df31ae28
xyl_t9 <dbsrjs1224@gmail.com>
2024-02-14 16:42:28 +0900

    02/14

--- Assets/1.Script/Game/BoardManager.cs
+++ Assets/1.Script/Game/BoardManager.cs
@@ -20,6 +20,8 @@ public class BoardManager : Singleton<BoardManager>
     {
         CreateBeadBG();
         CreateBead();
+
+        BeadBoradCheck();
     }
 
     /// <summary>
@@ -49,12 +51,22 @@ public class BoardManager : Singleton<BoardManager>
     void CreateBead()  //물약 생성
     {
         int y = 0;
+        int count = 0;
         for (int i = 0; i < transform.childCount; i++)
         {
             Bead b = Instantiate(bead, transform.GetChild(i));
             b.SetBead(Random.Range(0, (int)BeadType.Dark + 1));
             beads[y].Add(b);
-            if((i + 1) % width == 0)
+            /*
+            if (beads[y][-1].Type == beads[y][x].Type)
+            {
+                count++;
+            }
+            if (count == 2)
+            {
+                beads[y][x].SetBead(Random.Range(0, (int)BeadType.Dark + 1));
+            }*/
+            if ((i + 1) % width == 0)
                 y++;
         }
         //일차하는 항목이 있는지 확인 한 다음 없다면 다시 생성
@@ -75,51 +87,6 @@ public class BoardManager : Singleton<BoardManager>
                 check[i].Add(false);
             }
         }
-
-        /*// 가로 및 세로 체크
-        for (int i = 0; i < beads.Count; i++)
-        {
-            int horizontalCheckCnt = 0;
-            int verticalCheckCnt = 0;
-
-            for (int j = 0; j < beads[i].Count; j++)
-            {
-                BeadType horizontalBeadType = beads[i][j].Type;
-                BeadType verticalBeadType = beads[j][i].Type;
-
-                if (j + 1 < beads[i].Count && horizontalBeadType == beads[i][j + 1].Type)
-                {
-                    horizontalCheckCnt++;
-                }
-                else
-                {
-                    if (horizontalCheckCnt >= 2)
-                    {
-                        for (int delcnt = j; delcnt >= j - horizontalCheckCnt; delcnt--)
-                        {
-                            check[i][delcnt] = true;
-                        }
-                    }
-                    horizontalCheckCnt = 0;
-                }
-
-                if (j + 1 < beads.Count && verticalBeadType == beads[j + 1][i].Type)
-                {
-                    verticalCheckCnt++;
-                }
-                else
-                {
-                    if (verticalCheckCnt >= 2)
-                    {
-                        for (int delcnt = j; delcnt >= j - verticalCheckCnt; delcnt--)
-                        {
-                            check[delcnt][i] = true;
-                        }
-                    }
-                    verticalCheckCnt = 0;
-                }
-            }
-        }*/
 
         // 가로 체크
         for (int i = 0; i < beads.Count; i++)
@@ -147,13 +114,13 @@ public class BoardManager : Singleton<BoardManager>
         }
 
         // 세로 체크
-        for (int i = 0; i < width; i++) // 8
+        for (int i = 0; i < width; i++) //7
         {
             int checkCnt = 0;
-            for (int j = 0; j < height - 1; j++)
+            for (int j = 0; j < height - 1; j++)    //8
             {
                 BeadType bType = beads[j][i].Type;
-                if (i + 1 < beads[j].Count && bType == beads[j + 1][i].Type)
+                if (bType == beads[j + 1][i].Type)
                 {
                     checkCnt++;
                 }
@@ -219,17 +186,22 @@ public class BoardManager : Singleton<BoardManager>
         else
         {
             // 구슬 데이터 리플레쉬
+            bool isReflush = false;
             for (int i = 0; i < width; i++) // 8
             {
                 for (int j = 0; j < height; j++)
                 {
                     if (beads[j][i].gameObject.activeInHierarchy == false)
                     {
+                        isReflush = true;
                         beads[j][i].gameObject.SetActive(true);
                         beads[j][i].SetBead(Random.Range(0, (int)BeadType.Dark + 1));
                     }
                 }
             }
+
+            if (isReflush)
+                BeadBoradCheck();
         }
     }
 
